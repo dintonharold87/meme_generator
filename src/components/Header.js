a9aa199821cb65b00f9f9075d910a58362d1b265
dinton harold <dintonharold@gmail.com>
2022-04-25 00:10:10 +0300

    Finshed styling the meme component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,26 +2,25 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div
-      className="inline-flex items-end justify-start pl-16 pr-20 pt-11 pb-6 bg-purple-800 w-full"
-      
-    >
-      <div
-        className="flex items-center justify-center"
-        
-      >
-        <div className="flex items-center justify-center">
-          <img
-            className="h-auto w-1/4"
-            src={'../images/Troll Face.png'} alt="troll face"
-          />
-          <p className="text-3xl font-bold text-white">Meme Generator</p>
+    <section className="container mx-auto">
+      <div className="inline-flex items-end justify-start pl-16 pr-20 pt-11 pb-6 bg-purple-800 w-full">
+        <div className="flex items-center justify-center space-between">
+          <div className="flex items-center justify-center">
+            <img
+              className="h-auto w-1/4"
+              src={"../images/Troll Face.png"}
+              alt="troll face"
+            />
+            <p className="text-xl md:text-3xl font-bold text-white">
+              Meme Generator
+            </p>
+          </div>
+          <p className="text-lg md:text-3xl font-medium  text-white">
+            React Course - Project 3
+          </p>
         </div>
-        <p className="text-2xl font-medium text-right text-white">
-          React Course - Project 3
-        </p>
       </div>
-    </div>
+    </section>
   );
 }
 
--- src/components/Memes.js
+++ src/components/Memes.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Meme = () => {
   return (
-    <div>Memes</div>
-  )
-}
+    <section>
+      <form className="container mx-auto w-full max-w-lg mt-10">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder="Top Text"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="Bottom text"
+            />
+          </div>
+          <button
+            type="button"
+            className="w-full mt-5 text-white bg-purple-800 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
+          >
+            Get a New Meme image
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
 export default Meme;
