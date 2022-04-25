5e358a950c1a42cd33a32bbe8419b3fa83106a7f
dinton harold <dintonharold@gmail.com>
2022-04-26 00:02:09 +0300

    Added memes to the memesData file

--- src/components/Memes.js
+++ src/components/Memes.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import memesData from "./memesData";
 const Meme = () => {
+  // Function to get a random image from the `memesData` array
+  //     when the "new meme image" button is clicked.
+  function getMemeImage() {
+    // acccessing the array where the memes are stored in the memesData file and storing them in a constant called memesArray
+    const memesArray = memesData.data.memes;
+    // getting a random number(random meme) from the array and using that index to get a random url
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log("clicked");
+  }
   return (
     <section>
-      <form className="container mx-auto w-full max-w-lg mt-10">
+      <div className="container mx-auto w-full max-w-lg mt-10">
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <input
@@ -24,11 +34,12 @@ const Meme = () => {
           <button
             type="button"
             className="w-full mt-5 text-white bg-purple-800 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
+            onClick={getMemeImage}
           >
             Get a New Meme image
           </button>
         </div>
-      </form>
+      </div>
     </section>
   );
 };
